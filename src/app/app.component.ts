3211380a9ffffe34ebf52f486df6bf25460eae96
Bbylumi <odetoyinbopelumi42@gmail.com>
2024-11-12 13:55:04 +0100

    churchreport

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ReportComponent } from './report/report.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
