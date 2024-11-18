59bfb527eff2d35e65c38711c9fff2a4caadbeb6
Bbylumi <odetoyinbopelumi42@gmail.com>
2024-11-18 17:51:51 +0100

    place holder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ReportComponent } from './report/report.component';
+import { ReportComponent } from "./report/report.component"; 
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';  // Importing MatProgressSpinnerModule
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatProgressSpinnerModule, ReportComponent // Adding MatProgressSpinnerModule here
+  ]
+})
+export class ReportModule { }
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReportComponent],
+  imports: [ReportComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'report-sheet';
 }
+
+ 
--- src/app/report/report.component.ts
+++ src/app/report/report.component.ts
@@ -1,12 +1,221 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ], 
   templateUrl: './report.component.html',
-  styleUrl: './report.component.css'
+  styleUrls: ['./report.component.css'],
 })
-export class ReportComponent {
 
+
+export class ReportComponent{
+
+  
+  parish: string = 'Emmanuel Parish';
+  month: string = 'December';
+  year: number = 2024;
+  area: string = 'Faith-Mega Area';
+  
+
+  // table data
+  first_sunday: string = '2024-11-03';
+  men: number = 10;
+  women: number = 15;
+  children: number = 20;
+  total: number = 45;
+  preacher: string = ' Pastor Bankole';
+  new: number = 5;
+  guest: number = 2;
+
+  // first tuesday data
+  tuesday_men: number = 4;
+  tuesday_women: number = 5;
+  tuesday_children: number = 2;
+  tuesday_total: number = 11;
+  tuesday_date: string = '2024-11-05';
+
+  //tursday data
+  thursday_date: string = '2024-11-07';
+  thursday_men: number = 4;
+  thursday_women: number = 5;
+  thursday_children: number = 2;
+  thursday_total: number = 11;
+  thursday_preacher: string = 'sis victoria';
+
+  //second sunday
+  seconds = [
+    {
+      date: "2024-11-10",
+      serviceType: 'Sunday Morning',
+      men: "20",
+      women: "10",
+      children: '5',
+      total: '35',
+      new: '2',
+      guest: '1'
+    },
+    {
+      date: '2024-11-12',
+      serviceType: 'Tuesday and Wednesday',
+      men: "4",
+      women: "5",
+      children: "2",
+      total: "11",
+      preacher: "Pastor Bankole"
+    },
+    {
+      date: '2024-11-14',
+      serviceType: 'Thursday',
+      men: '2',
+      women: "3",
+      children: "5",
+      total: "10",
+      preacher: ''
+    }
+  ];
+  // second_sunday: string = '2024-10-10';
+  // second_men: number = 10;
+  // second_women: number = 15;
+  // second_children: number = 20;
+  // second_total: number = 45;
+  // second_new: number = 5;
+  // second_guest: number = 2; 
+
+  thirds = [
+    {
+      date: "2024-11-17",
+      serviceType: 'Sunday Morning',
+      men: "20",
+      women: "10",
+      children: '5',
+      total: '35',
+      preacher: 'Pastor Bankole',
+      new: '2',
+      guest: '1'
+    },
+
+  
+    {
+      date: '2024-11-19',
+      serviceType: 'Tuesday and Wednesday',
+      men: "4",
+      women: "5",
+      children: "2",
+      total: "11",
+      preacher: "Pastor Bankole"
+    },
+
+    
+    {
+      date: '2024-11-21',
+      serviceType: 'Thursday',
+      men: "4",
+      women: "5",
+      children: "2",
+      total: "11",
+      preacher: "Pastor Bankole"
+    }
+  ]
+
+
+  ///Fourth sunday
+  fourths = [
+    {
+      date: "2024-11-24",
+      serviceType: 'Sunday Morning',
+      men: "20",
+      women: "10",
+      children: '5',
+      total: '35',
+      preacher: 'Pastor Bankole',
+      new: '2',
+      guest: '1'
+    },
+
+  
+    {
+      date: '2024-11-26',
+      serviceType: 'Tuesday and Wednesday',
+      men: "4",
+      women: "5",
+      children: "2",
+      total: "11",
+      preacher: "Pastor Bankole"
+    },
+
+    
+    {
+      date: '2024-11-28',
+      serviceType: 'Thursday',
+      men: "4",
+      women: "5",
+      children: "2",
+      total: "11",
+      preacher: "Pastor Bankole"
+    }
+  ]
+
+
+  //fifth sunday
+  fifths = [
+    {
+      date: "",
+      serviceType: 'Sunday Morning',
+      men: "",
+      women: "",
+      children: '',
+      total: '',
+      preacher: '',
+      new: '',
+      guest: ''
+    },
+    {
+      date: '',
+      serviceType: 'Tuesday and Wednesday',
+      men: "",
+      women: "",
+      children: "",
+      total: "",
+      preacher: ""
+    },
+    {
+      date: '',
+      serviceType: 'Thursday',
+      men: "",
+      women: "",
+      children: "",
+      total: "",
+      preacher: ""
+    }
+  ]
+  averages = [
+    {
+      date: "",
+      serviceType: 'Tuesday',
+      men: "",
+      women: "",
+      children: '',
+    },
+    {
+      date: '',
+      serviceType: 'Thursday',
+      men: "",
+      women: "",
+      children: "",
+      total: "",
+      preacher: ""
+    },
+    {
+      date: '',
+      serviceType: 'Sunday',
+      men: "",
+      women: "",
+      children: "",
+      total: "",
+      preacher: ""
+    }
+  ]
 }
